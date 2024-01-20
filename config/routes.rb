9f501b358ed25f2902aea2a21a385f2884bb62e6
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2024-01-20 12:06:04 +0100

    Resolve changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/', to: 'users#index', as: :users
-  get '/users/:id', to: 'users#show', as: :user
-  get '/users/:id/posts', to: 'posts#index', as: :user_posts
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show], controller: 'user_posts'
+  end
 end
